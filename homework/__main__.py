6b011cc98d8d212351affb7a3a74fa4506c42aaa
SaraBalbin <51859641+SaraBalbin@users.noreply.github.com>
2025-05-17 08:37:48 -0500

    Solucion taller 4

--- homework/__main__.py
+++ homework/__main__.py
@@ -1,11 +1,8 @@
 """Entry point for the homework package."""
 
-# python3 -m homework 10 data/raw data/input data/output
+# python -m homework data/input data/output
 
-from .src.main import main
+from .src.main import WordCountApp
 
 if __name__ == "__main__":
-    main()
-    
-    
-    
+    WordCountApp().run()
--- homework/src/main.py
+++ homework/src/main.py
@@ -1,25 +1,41 @@
-import argparse
+
+from homework.src._internals.count_words import CountWordsMixin
+from homework.src._internals.parse_args import ParseArgsMixin
+from homework.src._internals.preprocess_lines import PreprocessLinesMixin
+from homework.src._internals.read_all_lines import ReadAllLinesMixin
+from homework.src._internals.split_into_words import SplitIntoWordsMixin
+from homework.src._internals.write_word_counts import WriteWordCountsMixin
 
 
-def parse_args():
+class WordCountApp(
+    ParseArgsMixin,
+    ReadAllLinesMixin,
+    PreprocessLinesMixin,
+    SplitIntoWordsMixin,
+    CountWordsMixin,
+    WriteWordCountsMixin,
+):
+    def __init__(self):
+        self.input_folder = None
+        self.output_folder = None
+        self.lines = None
+        self.preprocessed_lines = None
+        self.words = None
+        self.word_counts = None
 
-    parser = argparse.ArgumentParser(description="Count words in files.")
+    def run(self):
 
-    parser.add_argument(
-        "input",
-        type=str,
-        help="Path to the input folder containing files to process",
-    )
-    parser.add_argument(
-        "output",
-        type=str,
-        help="Path to the output folder for results",
-    )
-
-    parsed_args = parser.parse_args()
-
-    return parsed_args.input, parsed_args.output
+        self.parse_args()
+        self.read_all_lines()
+        self.preprocess_lines()
+        self.split_into_words()
+        self.count_words()
+        self.write_word_counts()
 
 
-def main():
-   pass
+if __name__ == "__main__":
+    WordCountApp().run()
+
+    
+    
+    
